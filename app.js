cf5e03966cc752405d137f4b2227e362e2a46f7a
AntoDaiva <antoniusdaiva@gmail.com>
2024-06-12 01:07:13 +0700

    feat: populate db

--- app.js
+++ app.js
@@ -16,10 +16,12 @@ const pool = new Pool({
   database: process.env.PG_DATABASE,
 });
 
-const redisClient = redis.createClient({
-  host: process.env.REDIS_HOST,
-  port: process.env.REDIS_PORT,
-});
+// const redisClient = redis.createClient({
+//   host: process.env.REDIS_HOST,
+//   port: process.env.REDIS_PORT,
+// });
+
+const redisClient = redis.createClient();
 
 redisClient.on('error', (err) => {
   console.error('Redis error: ', err);
@@ -29,12 +31,30 @@ app.use(express.json());
 
 // Import routes
 const articleRoutes = require('./routes/articleRoutes');
-const categoryRoutes = require('./routes/categoryRoutes');
-const userRoutes = require('./routes/userRoutes');
+// const categoryRoutes = require('./routes/categoryRoutes');
+// const userRoutes = require('./routes/userRoutes');
 
 app.use('/articles', articleRoutes);
-app.use('/categories', categoryRoutes);
-app.use('/users', userRoutes);
+// app.use('/categories', categoryRoutes);
+// app.use('/users', userRoutes);
+
+async function someAsyncFunction() {
+  // Your code here
+  await redisClient.connect();
+
+  await redisClient.set('key', 'value');
+  const value = await redisClient .get('key');
+
+  await redisClient.hSet('user-session:123', {
+    name: 'John',
+    surname: 'Smith',
+    company: 'Redis',
+    age: 29
+  })
+
+  let userSession = await redisClient.hGetAll('user-session:123');
+  console.log(JSON.stringify(userSession, null, 2));
+}
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
--- services/articleService.js
+++ services/articleService.js
@@ -1,32 +1,36 @@
 const { pool, redisClient } = require('../app');
 const { promisify } = require('util');
-const getAsync = promisify(redisClient.get).bind(redisClient);
-const setAsync = promisify(redisClient.set).bind(redisClient);
+// const getAsync = promisify(redisClient.get).bind(redisClient);
+// const setAsync = promisify(redisClient.set).bind(redisClient);
 
 const getArticleById = async (id) => {
   const cacheKey = `article:${id}`;
-  let article = await getAsync(cacheKey);
+  let article = await redisClient.get(cacheKey);
 
   if (article) {
-    return JSON.parse(article);
+    console.log('cache hit!');
+    return JSON.parse(article);   
   } else {
+    console.log('cache miss!');
     const result = await pool.query('SELECT * FROM articles WHERE id = $1', [id]);
     article = result.rows[0];
-    await setAsync(cacheKey, JSON.stringify(article), 'EX', 3600);
+    await redisClient.set(cacheKey, JSON.stringify(article), 'EX', 3600);
     return article;
   }
 };
 
 const getLatestArticles = async () => {
   const cacheKey = 'articles:latest';
-  let articles = await getAsync(cacheKey);
+  let articles = await redisClient.get(cacheKey);
 
   if (articles) {
+    console.log('cache hit!');
     return JSON.parse(articles);
   } else {
-    const result = await pool.query('SELECT * FROM articles ORDER BY published_date DESC LIMIT 10');
+    console.log('cache miss!');
+    const result = await pool.query('SELECT * FROM article ORDER BY publish_date DESC LIMIT 10');
     articles = result.rows;
-    await setAsync(cacheKey, JSON.stringify(articles), 'EX', 600);
+    await redisClient.set(cacheKey, JSON.stringify(articles), 'EX', 3600);
     return articles;
   }
 };
